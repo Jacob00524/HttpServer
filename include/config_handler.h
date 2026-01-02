ce4a7c957e27ff90ed6674dc28592eca201a693c
Jacob Czekalla <jczekalla@codeweavers.com>
2026-01-02 16:42:30 -0500

    Add write_file helper.

--- include/config_handler.h
+++ include/config_handler.h
@@ -4,3 +4,4 @@ int read_config(char *path, Server_Settings *settings);
 int create_config(char *config_path, Server_Settings *settings);
 int create_folder(char *name);
 int create_file(char *text, char *folder, char *name);
+int write_file(char *text, char *folder, char *name);
